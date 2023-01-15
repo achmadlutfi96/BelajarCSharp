32034dace9272bbc4332b4a00245578d2493a0fb
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2023-01-15 17:51:26 +0700

    mencoba class dan variable

--- Bab2VariabelDanTipeData/Program.cs
+++ Bab2VariabelDanTipeData/Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace Bab2VariabelDanTipeData
 {
+    class Manusia
+    {
+        string name;
+
+        public Manusia(string name)
+        {
+            this.name = name;
+        }
+
+        public void calledName()
+        {
+            Console.WriteLine("Hallo, " +this.name);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Achmad Lutfi");
+            Console.Write("Masukkan nama:");
+            string name = Console.ReadLine();
+            Manusia m = new Manusia(name);
+            m.calledName();
         }
     }
 }
