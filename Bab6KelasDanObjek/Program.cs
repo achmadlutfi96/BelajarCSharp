463fb0356699b98356184adbdda3c68f037ce6e1
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2023-01-16 20:07:12 +0700

    Tambah bab 4, 5 dan 6

---
+++ Bab6KelasDanObjek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bab6KelasDanObjek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
