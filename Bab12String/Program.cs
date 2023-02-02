2bad2312f7799164116aa1af4454b402969c23d8
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2023-02-02 20:05:16 +0700

    Menambahkan 2 bab baru

---
+++ Bab12String/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bab12String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Pemrograman C#";
+            // mengakses properti
+            Console.WriteLine(s.Length);
+
+            // memanggil metode
+            Console.WriteLine(s.ToUpper());
+            Console.WriteLine(s.Substring(12,2));
+
+
+            // array dari type karakter
+            char[] arrchar = {'.', 'N','E','T'};
+
+            // melewatkan array ke dalam
+            // konstruktor string
+            string s2 = new string(arrchar);
+            Console.WriteLine("Microsoft "+s2);
+
+            // array dari type karakter
+            char[] arrchar2 = { 'M', 'i', 'c', 'r', 'o', 's', 'o', 'f', 't' };
+
+            string s3 = new string(arrchar2, 0, 5);
+            Console.WriteLine(s3);
+
+
+            // array dari tipe string
+            string[] bulan = { "Januari","Februari", "Maret",
+                                "April", "Mei", "Juni",
+                                "Juli", "Agustus", "September",
+                                "Oktober", "November", "Desember"};
+            // mengambil tanggal saat ini
+            DateTime tanggal = DateTime.Today;
+
+            // ekstrak bulan dari tanggal
+            int mm = tanggal.Month;
+
+            Console.WriteLine("Tanggal sekarang: " + tanggal);
+            Console.WriteLine("Tanggal sekarang: {0} {1} {2}", tanggal.Day, bulan[mm - 1], tanggal.Year);
+
+
+            // Penggunaan Kelas StringBuilder
+            StringBuilder sb= new StringBuilder(99);
+            sb.Append("Pemrogramman ");
+            sb.Append("Visual C#");
+
+            // menampilkan string
+            Console.WriteLine(sb);
+            int kapasitas = sb.Capacity;
+            Console.WriteLine(kapasitas);
+
+            Console.WriteLine("sb \t\t: "+sb);
+            Console.WriteLine("sb.Capacity \t: " + sb.Capacity);
+            Console.WriteLine("sb.MaxCapacity \t: " + sb.MaxCapacity);
+            Console.WriteLine("Int32.MaxValue \t: " + Int32.MaxValue);
+            Console.WriteLine("sb.Length \t: " + sb.Length);
+        }
+    }
+}
---
+++ Bab13DelegasiMetodeTanpaNamaDanEkspresiLambda/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bab13DelegasiMetodeTanpaNamaDanEkspresiLambda
+{
+    // mendeklarasikan tipe delegasi
+    delegate double OperasiBilangan(double a, double b);
+    public delegate void BalikDanCetak();
+    internal class Program
+    {
+        static double Tambah(double bil1, double bil2)
+        {
+            return bil1 + bil2;
+        }
+        static double Kurang(double bil1, double bil2)
+        {
+            return bil1 - bil2;
+        }
+        static double Kali(double bil1, double bil2)
+        {
+            return bil1 * bil2;
+        }
+        static double Bagi(double bil1, double bil2)
+        {
+            return bil1 / bil2;
+        }
+        static void Main(string[] args)
+        {
+            // mendeklarasikan variabel lokal
+            int a = 10, b = 3;
+
+            // mendeklarasikan referensi ke tipe delegasi
+            OperasiBilangan objDel;
+
+            // membuat objek delegasi
+            // untuk menunjuk ke metode Tambah()
+            objDel = new OperasiBilangan(Tambah);
+            
+            // memanggil metode yang ditunjuk
+            // oleh objek delegasi melalui objDel
+            Console.WriteLine("{0} + {1} = {2}", a, b, objDel(a,b));
+
+            // membuat objek delegasi
+            // untuk menunjuk ke metode Kurang()
+            objDel = new OperasiBilangan(Kurang);
+            Console.WriteLine("{0} - {1} = {2}", a, b,objDel(a,b));
+
+            // membuat objek delegasi
+            // untuk menunjuk ke metode Kali()
+            objDel = new OperasiBilangan(Kali);
+            Console.WriteLine("{0} * {1} = {2}", a, b, objDel(a, b));
+
+            // membuat objek delegasi
+            // untuk menunjuk ke metode Bagi()
+            objDel = new OperasiBilangan(Bagi);
+            Console.WriteLine("{0} / {1} = {2}", a, b, objDel(a, b));
+
+
+            // Metode Tanpa Nama
+            StringBuilder sb = new StringBuilder("TFOSORCIM");
+            BalikDanCetak objDelegasi = delegate
+            {
+                char[] temp = new char[sb.Length];
+                int j = 0;
+                for (int i = sb.Length - 1; i >= 0; i--, j++)
+                {
+                    temp[j] = sb[i];
+                }
+                sb.Clear();
+                sb.Append(new String(temp));
+                Console.WriteLine(sb.ToString());
+            };
+
+            Console.WriteLine("Mengeksekusi objDel:");
+            objDelegasi();
+        }
+    }
+}
